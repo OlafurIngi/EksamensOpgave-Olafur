800f67d4f1086142112a042c13855e74483b3df6
Olafur <olafuringieinarsson@gmail.com>
2021-11-29 18:46:15 +0100

    Update register.js

--- src/views/register.js
+++ src/views/register.js
@@ -2,17 +2,22 @@
 
 document.addEventListener("DOMContentLoaded", (event) => {
   // Here I define the form that is related in the html file
-  document.getElementById("form").addEventListener("submit", (event) => {
-    event.preventDefault();
+  const form = document.getElementById("form")
+  form.addEventListener("submit", (event) => {
+            // Prevent any default action
+            event.preventDefault();
 
+    // defining email and password by their ID in the form     
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
+    // Define the user parameter so we can use it in our fetch statement
     const user = {
       email: email,
       password: password,
     };
 
+    // Fetch the information of the user
     fetch("http://localhost:3000/users/create", {
       method: "POST",
       headers: {
@@ -20,14 +25,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
       },
       body: JSON.stringify(user),
     })
+
+
+      // If response is found go to login.html, if not, prompt error.
       .then((response) => response.json())
       .then((response) => {
         if (response) {
           location.href = "/login.html";
         }
       })
+      // Catch the error 
       .catch(() => {
-        window.alert("Der skete en fejl");
+        window.alert("Error, try again");
       });
   });
 });
